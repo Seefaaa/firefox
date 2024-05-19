3a10bc300e9a5db7f07b24a393ad8e8e72b9a00c
Sefa <sefa@outlook.ie>
2024-05-19 12:07:25 +0300

    Add deploy.cmd script for copying CSS files to Firefox profile

---
+++ deploy.cmd
@@ -0,0 +1,30 @@
+@echo off
+
+cd /d %~dp0
+
+SET FIREFOXPROFILE=%~1\chrome
+
+IF NOT EXIST %FIREFOXPROFILE% (
+	echo Profile directory does not exist
+	exit /b 1
+)
+
+IF EXIST %FIREFOXPROFILE%\userChrome.css (
+	del %FIREFOXPROFILE%\userChrome.css
+)
+
+copy userChrome.css %FIREFOXPROFILE%\userChrome.css 1>nul
+
+IF EXIST %FIREFOXPROFILE%\userContent.css (
+	del %FIREFOXPROFILE%\userContent.css
+)
+
+copy userContent.css %FIREFOXPROFILE%\userContent.css 1>nul
+
+IF EXIST %FIREFOXPROFILE%\userVariables.css (
+	del %FIREFOXPROFILE%\userVariables.css
+)
+
+copy userVariables.css %FIREFOXPROFILE%\userVariables.css 1>nul
+
+echo CSS files copied to %FIREFOXPROFILE%
